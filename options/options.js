43d013017d159ab1a58812e6a81e739822496d7d
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 23:17:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-waterfall-d-133414",
+  url: "https://little-waterfall-d-133414.botics.co",
+  api: "https://little-waterfall-d-133414.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
